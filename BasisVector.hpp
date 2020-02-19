852d4d4b3f9c2c62eed74b45fad3a3c4bfb04464
Abhinav Jhanwar <ajhanwar97@gmail.com>
2020-02-19 18:28:45 +0530

    changes to basis vector

--- BasisVector.hpp
+++ BasisVector.hpp
@@ -1,5 +1,7 @@
-#include <iostream>
-#include <type_traits>
+#include "Axis.hpp"
+#include "Integral.hpp"
+
+#include <array>
 
 template <typename T, typename Enable = void>
 class BasisVector;
@@ -7,16 +9,16 @@ class BasisVector;
 template <typename T>
 class BasisVector<T, typename std::enable_if_t<std::is_integral_v<T>>> {
  private:
-  T x;
-  T y;
-  T z;
+  std::array<Axis, 3> axes { Axes::kX, Axes::kY, Axes::kZ };
+  std::array<Integral<T>, 3> dimensions;
  public:
   BasisVector() = default;
-  BasisVector(T const& x, T const& y, T const& z) : x { x }, y { y }, z { z } { }
+  BasisVector(Integral<T> const& x, Integral<T> const& y, Integral<T> const& z) : dimensions { x, y, z  } { }
   void output_on(std::ostream& os) const {
-    os << "X: " << x << ", Y: " << y << ", Z: " << z;
+    for (size_t index { }; index < axes.size(); ++index)
+      os << axes.at(index) << ": " << dimensions.at(index) << '\n';
   }
-  friend std::ostream& operator<<(std::ostream& os, BasisVector<T> const& vec) {
+  friend std::ostream& operator<<(std::ostream& os, BasisVector const& vec) {
     vec.output_on(os);
     return os;
   }
--- basis_vector.cpp
+++ basis_vector.cpp
@@ -1,7 +1,7 @@
 #include "BasisVector.hpp"
 
 int main() {
-  BasisVector<int> bv {3, 4, 7};
+  BasisVector<int> bv {1, 4, 7};
   std::cout << "Vector dimensions: \n";
-  std::cout << bv << '\n';
+  std::cout << bv;
 }
