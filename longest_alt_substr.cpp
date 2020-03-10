2436b9c216c947171b5ebbc4c25a56a0d034c4f1
Abhinav Jhanwar <ajhanwar97@gmail.com>
2020-03-10 19:40:24 +0530

    Change solution

--- longest_alt_substr.cpp
+++ longest_alt_substr.cpp
@@ -5,55 +5,44 @@
 #include <iostream>
 #include <string>
 
-// Type of digit(odd or even)
-// Zero is considered even in this case
-// Neither is the initial type before iteration has started
-enum class Type {
-  kNeither,
-  kEven,
-  kOdd
-};
-
-// Function to return the type of digit(odd or even)
-Type type(const char digit_char) {
-  const uint8_t digit { static_cast<uint8_t>(digit_char - '0') };
-  return digit % 2 == 0 ? Type::kEven : Type::kOdd;
-}
-
+// Improved solution from:
+// https://reddit.com/r/cpp_questions/comments/fg8enw/longest_alternating_substring_code_challenge_how/fk3ta6g/
 std::string longestSubstring(const std::string& digits) {
   size_t final_length {};
-  size_t curr_length {};
-  std::string longest_substr;
-  std::string::const_iterator store_iter;
-  Type prev_type { Type::kNeither };
-  auto iter { digits.begin() };
-  for (; iter != digits.end(); ++iter) {
-    if (type(*iter) != prev_type) {
-      // Increment length if the current digit type is different
-      // to the previous digit type
+  size_t curr_length { 1 };
+  size_t last_index { digits.size() };
+  size_t store_index {};
+
+  auto different_type {
+                       [](int8_t digit) -> bool { return digit & 1; }
+  };
+  auto update_store_index {
+                           [&final_length, &store_index, &curr_length](size_t index) {
+                              if (curr_length > final_length) {
+                                final_length = curr_length;
+                                store_index = index;
+                              }
+                              // Reset the length counter when the previous
+                              // digit type is the same as the current one
+                              curr_length = 1;
+                            }
+  };
+
+  for (size_t index { 1 }; index < last_index; ++index) {
+    // Increment length if the current digit type is different
+    // to the previous digit type
+    // If the current digit type is the same as the previous digit type and if
+    // the length is greater than the length of the last recorded substring,
+    // store the new iterator pointing to the current longest substring
+    if (different_type(digits.at(index) - digits.at(index - 1)))
       ++curr_length;
-    } else {
-      // If the current digit type is the same as the previous digit type and if
-      // the length is greater than the length of the last recorded substring,
-      // store the new iterator pointing to the current longest substring
-      if (curr_length > final_length) {
-        final_length = curr_length;
-        store_iter = iter;
-      }
-      // Reset the length counter when the previous digit type
-      // is the same as the current digit type
-      curr_length = 1;
-    }
-    prev_type = type(*iter);
+    else
+      update_store_index(index);
   }
   // Check for the length of the substring at the end of the digits string
-  if (curr_length > final_length) {
-    final_length = curr_length;
-    store_iter = iter;
-  }
-  // Assign the longest found substring using the stored iterator
-  longest_substr = std::string(store_iter - final_length, store_iter);
-  return longest_substr;
+  update_store_index(last_index);
+  // Return the longest found substring using the stored iterator
+  return digits.substr(store_index - final_length, final_length);
 }
 
 int main() {
